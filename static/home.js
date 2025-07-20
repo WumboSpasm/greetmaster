bf5fe074519a4e4ead52e8a2774f05973ce607db
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2025-07-20 14:52:51 -0400

    Use IntersectionObserver for thumbnail grid

--- static/home.js
+++ static/home.js
@@ -1,7 +1,6 @@
 const thumbWidth = 100;
 const thumbHeight = 54;
 
-const thumbDisplayBuffer = 2;
 const thumbScrollBuffer = 20;
 const thumbLoadBuffer = 40;
 
@@ -12,17 +11,23 @@ let pageFullyLoaded = false;
 let loadingThumbs = false;
 let thumbsLoaded = 0;
 
-let displayThumbsTimer;
-let scrollTimer = null;
-let lastScrollPos = 0;
-
 const defaultSrc = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
 
 const getThumbColumns = () => Math.floor(thumbContainer.scrollWidth / thumbWidth);
 const getThumbRows = () => Math.ceil(thumbContainer.scrollHeight / thumbHeight);
 const getVisibleThumbRows = () => Math.ceil(thumbContainer.offsetHeight / thumbHeight);
 
-function loadThumbs(thumbRemainder, doDisplayThumbs) {
+const thumbObserver = new IntersectionObserver((entries, observer) => {
+	for (const entry of entries) {
+		const thumb = entry.target;
+		if (thumb.src == defaultSrc && entry.isIntersecting) {
+			thumb.src = thumb.dataset.src;
+			observer.unobserve(thumb);
+		}
+	}
+});
+
+function loadThumbs(thumbRemainder) {
 	loadingThumbs = true;
 	const thumbAmount = (getVisibleThumbRows() + thumbLoadBuffer) * getThumbColumns() + thumbRemainder;
 	let getUrl = `/get?count=${thumbAmount}&offset=${thumbsLoaded}&field=titles&field=thumbnail`;
@@ -41,49 +46,21 @@ function loadThumbs(thumbRemainder, doDisplayThumbs) {
 			thumb.setAttribute("height", thumbHeight);
 			thumb.setAttribute("title", greetings[id].titles.length > 0 ? greetings[id].titles[0].replace(/<br>/i, "\n") : "");
 			thumb.setAttribute("loading", "lazy");
+			thumbObserver.observe(thumb);
 			link.append(thumb);
 			thumbFragment.append(link);
 		}
 		thumbContainer.append(thumbFragment);
 		thumbsLoaded += realThumbAmount;
 		loadingThumbs = false;
-		if (doDisplayThumbs) displayThumbs();
 	});
 }
 
 function prepareLoadThumbs() {
-	prepareDisplayThumbs();
 	if (loadingThumbs || pageFullyLoaded) return;
 	const thumbRows = getThumbRows();
 	if (thumbContainer.scrollTop + thumbContainer.offsetHeight > (thumbRows - thumbScrollBuffer) * thumbHeight)
-		loadThumbs((thumbRows * getThumbColumns()) - thumbsLoaded, false);
-}
-
-function displayThumbs() {
-	const thumbColumns = getThumbColumns();
-	const thumbStart = thumbColumns * Math.max(0, Math.floor(thumbContainer.scrollTop / thumbHeight) - thumbDisplayBuffer);
-	const thumbEnd = thumbColumns * Math.ceil((thumbContainer.scrollTop + thumbContainer.offsetHeight) / thumbHeight + thumbDisplayBuffer);
-	const thumbList = thumbContainer.children;
-	for (let i = thumbStart; i < thumbEnd; i++) {
-		const thumb = thumbList.item(i)?.children.item(0);
-		if (!thumb || thumb.src != defaultSrc || thumb.dataset.src == "/data/") continue;
-		thumb.src = thumb.dataset.src;
-	}
-}
-
-function prepareDisplayThumbs() {
-	if (scrollTimer !== null) return;
-	scrollTimer = setTimeout(() => {
-		const scrollVelocity = Math.abs(thumbContainer.scrollTop - lastScrollPos);
-		if (scrollVelocity > 250) {
-			clearTimeout(displayThumbsTimer);
-			displayThumbsTimer = setTimeout(displayThumbs, 300);
-		}
-		else
-			displayThumbs();
-		lastScrollPos = thumbContainer.scrollTop;
-		scrollTimer = null;
-	}, 200);
+		loadThumbs((thumbRows * getThumbColumns()) - thumbsLoaded);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -91,5 +68,5 @@ document.addEventListener("DOMContentLoaded", () => {
 	thumbContainer.addEventListener("scroll", prepareLoadThumbs);
 	new ResizeObserver(prepareLoadThumbs).observe(thumbContainer);
 	thumbFragment = document.createDocumentFragment();
-	loadThumbs(0, true);
+	loadThumbs(0);
 });
